d885a03e0aa7f643e001e1b6be18b6b667833f01
moein <moeinrezaie516@gmail.com>
2025-01-04 23:31:55 +0330

    create form acc

--- multi_shop/settings.py
+++ multi_shop/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',      #my app
-    'home.apps.HomeConfig'
+    'home.apps.HomeConfig',
+    'account.apps.AccountConfig'
 ]
 
 MIDDLEWARE = [
