79d360cd26f9c56938a7d51f32eade40c2cbff22
moein <moeinrezaie516@gmail.com>
2025-01-11 19:56:55 +0330

    register Done

--- account/models.py
+++ account/models.py
@@ -68,6 +68,7 @@ class User(AbstractBaseUser):
 
 
 class Otp(models.Model):
+    token = models.CharField(max_length=20, null=True, blank=True)
     phone = models.CharField(max_length=11)
     code = models.SmallIntegerField()
     expiration_date = models.DateTimeField(auto_now_add=True)
--- account/views.py
+++ account/views.py
@@ -5,6 +5,7 @@ from django.views.generic import TemplateView
 from .forms import LoginForm, RegisterForm, CheckOtpForm
 from django.contrib.auth import authenticate, login
 import requests
+from django.utils.crypto import get_random_string
 from .models import Otp, User
 import ghasedakpack
 sms = ghasedakpack.Ghasedak(apikey='495df957a36aaa2f58a051a28dbcc17b96f1b9a6b85ca9f075a7470edd8b29c1seobCY2G5nh6AvzP')
@@ -49,9 +50,10 @@ class RegisterView(View):
             randcode = randint(1000,9999)
             sms.verification({'receptor': cd["phone"], 'type': '1', 'template': 'Ghasedak', 'param1': randcode})
 
-            Otp.objects.create(phone=cd['phone'], code=randcode)
-            print(randcode)
-            return redirect(reverse('account:check_otp') + f'?phone={cd["phone"]}')
+            token = get_random_string(length=100)
+            Otp.objects.create(phone=cd['phone'], code=randcode, token=token)
+
+            return redirect(reverse('account:check_otp') + f'?token={token}')
 
 
         else:
@@ -66,15 +68,18 @@ class CheckOtpView(View):
         return render(request, 'account/check_otp.html', {'form': form})
 
     def post(self, request):
-        phone = request.GET.get('phone')
+        token = request.GET.get('token')
         form = CheckOtpForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            if Otp.objects.filter(code=cd['code'], phone=phone).exists():
-                user = User.objects.create_user(phone=phone)
+            if Otp.objects.filter(code=cd['code'],token=token).exists():
+                otp = Otp.objects.get(token=token)
+                user = User.objects.create_user(phone=otp.phone)
                 login(request, user)
                 return redirect('/')
         else:
             form.add_error("phone", "invalid data")
 
         return render(request, "account/check_otp.html", {'form': form})
+
+
