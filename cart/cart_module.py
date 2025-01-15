6d21138d57111f8d4f3a7c29df822ff07d574770
moein <moeinrezaie516@gmail.com>
2025-01-15 22:57:17 +0330

    yield

--- cart/cart_module.py
+++ cart/cart_module.py
@@ -1,4 +1,4 @@
-
+from product.models import Product
 
 CART_SESSION_ID = 'cart'
 
@@ -10,6 +10,15 @@ class Cart:
             cart = self.session[CART_SESSION_ID] = {}
 
         self.cart = cart
+
+
+    def __iter__(self):
+        cart = self.cart.copy()
+
+        for item in cart.values():
+            item['product'] = Product.objects.get(id=int(item['id']))
+            item['total'] = int(item['quantity']) * int(item['price'])
+            yield item
 
 
     def unique_id_generator(self, id, color, size):
