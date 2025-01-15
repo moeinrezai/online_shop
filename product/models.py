e4c1e70d2f6427919567f9f75412fdbee0edcbe4
moein <moeinrezaie516@gmail.com>
2025-01-15 18:15:14 +0330

    product 501

--- product/models.py
+++ product/models.py
@@ -30,3 +30,12 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+
+
+class Information(models.Model):
+    product = models.ForeignKey(Product,null=True, blank=True, on_delete=models.CASCADE, related_name="information")
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text[:50]
