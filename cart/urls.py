29b192b48f990e44d6e8f44919c46168938943ef
moein <moeinrezaie516@gmail.com>
2025-01-19 00:23:14 +0330

    order detail view

--- cart/urls.py
+++ cart/urls.py
@@ -4,5 +4,7 @@ app_name = 'cart'
 urlpatterns = [
     path('detail', views.CartDetailView.as_view(), name='cart_detail'),
     path('add/<int:pk>', views.CartAddView.as_view(), name='cart_add'),
-    path('delete/<str:id>', views.CartDeleteView.as_view(), name='cart_delete')
+    path('delete/<str:id>', views.CartDeleteView.as_view(), name='cart_delete'),
+    path('order/<int:id>', views.OrderDetailView.as_view(), name='order_detail'),
+    path('/order/add', views.OrderCreationView.as_view(), name='order_creation'),
 ]
