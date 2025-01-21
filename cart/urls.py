540f8b86b2199b636a6faeba5daee303857f2297
moein <moeinrezaie516@gmail.com>
2025-01-21 19:57:01 +0330

    pay

--- cart/urls.py
+++ cart/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('add/<int:pk>', views.CartAddView.as_view(), name='cart_add'),
     path('delete/<str:id>', views.CartDeleteView.as_view(), name='cart_delete'),
     path('order/<int:pk>', views.OrderDetailView.as_view(), name='order_detail'),
-    path('/order/add', views.OrderCreationView.as_view(), name='order_creation'),
+    path('order/add', views.OrderCreationView.as_view(), name='order_creation'),
+    path('applydiscount/<int:pk>', views.ApplyDiscountCodeView.as_view(), name='apply_discount'),
 ]
